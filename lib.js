14df27a9e369fa6c9399b5ef2775f04af63448cf
Общий профиль ptpit <ptpit@pl1.edu>
2021-04-10 09:49:56 +0500

    moving

---
+++ lib.js
@@ -0,0 +1,70 @@
+document.onkeydown = checkButton;
+
+function checkButton(event) {
+
+    if (event.keyCode === 38) {
+        // ArrowUp: 38
+        if (cube.y - cube.step > map.y) {
+            cube.y -= cube.step;
+        }
+
+        else {
+            cube.y = map.y;
+        }
+    }
+
+    if (event.keyCode === 40) {
+        // ArrowDown 40
+        if (cube.y + cube.height + cube.step < map.width + map.x) {
+            cube.y += cube.step;
+        }
+
+        else {
+            cube.y = map.width + map.x - cube.height;
+        }
+    }
+
+    if (event.keyCode === 37) {
+        // ArrowLeft: 37
+        if (cube.x - cube.step > map.x) {
+            cube.x -= cube.step;
+        }
+
+        else {
+            cube.x = map.x;
+        }
+    }
+
+    if (event.keyCode === 39) {
+        // ArrowRight: 39
+        if (cube.x + cube.width + cube.step < map.height + map.y) {
+            cube.x += cube.step;
+        }
+
+        else {
+            cube.x = map.height + map.y - cube.width;
+        }
+    }
+    
+    renderCube(cube);
+}
+
+function renderCube(cube) {
+    document.getElementById(cube.id).style.top = cube.y + 'px';
+    document.getElementById(cube.id).style.left = cube.x + 'px';
+    document.getElementById(cube.id).style.transition = 'ease ' +  cube.trn + 's';
+    document.getElementById(cube.id).style.width = cube.width + 'px';
+    document.getElementById(cube.id).style.height = cube.height + 'px';
+    document.getElementById(cube.id).style.backgroundColor = cube.color;
+
+    // Вывод координат (В.П - верхний правый угол / Н.Л - нижний левый угол)
+    console.log("(В.П) x,y = " + cube.x + "/" + cube.y + " | " + "(Н.Л) x,y = " + (cube.x + cube.step) + "/" + (cube.y + cube.step))
+}
+
+function renderMap(map) {
+    document.getElementById('map').style.top = map.y + 'px';
+    document.getElementById('map').style.left = map.x + 'px';
+    document.getElementById('map').style.width = map.width + 'px';
+    document.getElementById('map').style.height = map.height + 'px';
+    document.getElementById('map').style.backgroundColor = map.color;
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+let cube = {
+    x: 300,
+    y: 300,
+    id: "cube",
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: 'black'
+}
+
+let map = {
+    x: 100,
+    y: 100,
+    width: 470,
+    height: 470,
+    color: '#f5f5f5'
+}
+
+renderMap(map); //Рендер карты
+renderCube(cube); //Рендер куба
